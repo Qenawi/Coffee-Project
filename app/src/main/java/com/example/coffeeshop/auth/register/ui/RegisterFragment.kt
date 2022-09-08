762c1715ac0a67f7f01d091086d42045ab43a36c
Eslam Hamouda Ali <69871727+EslamHamouda@users.noreply.github.com>
2022-09-08 17:47:54 +0200

    resolving conflicts

--- app/src/main/java/com/example/coffeeshop/auth/register/ui/RegisterFragment.kt
+++ app/src/main/java/com/example/coffeeshop/auth/register/ui/RegisterFragment.kt
@@ -6,9 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.coffeeshop.R
+import com.example.coffeeshop.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
-
+    lateinit var binding: FragmentRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,7 +20,8 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding= FragmentRegisterBinding.inflate(inflater)
+        return binding.root
     }
 
 
